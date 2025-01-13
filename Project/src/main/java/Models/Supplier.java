c07de89914c6a6b0984460f7fdbddd98a02f9615
Endri Baku <endri.bakual@gmail.com>
2025-01-13 15:44:06 +0100

    Changed supplier

--- Project/src/main/java/Models/Supplier.java
+++ Project/src/main/java/Models/Supplier.java
@@ -1,4 +1,28 @@
 package Models;
 
-public class Supplier {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Supplier implements Serializable {
+    String supplierID;
+    String name;
+    ArrayList<Item> suppliedItems;
+
+    public String getSupplierID() {return supplierID;}
+    public String getName() {return name;}
+    public ArrayList<Item> getSuppliedItems() {return suppliedItems;}
+
+    public void setSupplierID(String supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public void setName(String name) {this.name = name;}
+
+    public void setSuppliedItems(ArrayList<Item> suppliedItems) {this.suppliedItems = suppliedItems;}
+
+    public void addSuppliedItem(Item item)
+    {
+        if (suppliedItems == null) suppliedItems = new ArrayList<>();
+        suppliedItems.add(item);
+    }
 }
